43683684dc46ee66ad993f2e2d449e21aafaf608
MSHECATE <109221471+MSHECATE@users.noreply.github.com>
2022-11-21 18:38:29 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,38 +1,18 @@
-menuListArray = 
-[
-"Pizza Vegetariana",
-"Pizza de frango",
-"Pizza portuguesa",
-"Pizza de quatro queijos",
-"Pizza doce",
-"Pizza de kebab",
-"Pizza de banana"
-];
+menuListArray = ["Pizza Vegetariana",//adicionar mais itens
+                    ];
 
 function getMenu(){
-var htmldata ="";
-menuListArray.sort();
-for(var i=0;i<menuListArray.length;i++){
-   htmldata=htmldata+ menuListArray[i] + '<br>'
-}
-document.getElementById("displaymenu").innerHTML = htmldata;
+var htmldata;
+//Completar o código
 }
 
 function addItem(){
 var htmldata;
-var imgtags='<img id="im1" src="images/pizzaImg.png"/>'
 var item=document.getElementById("addItem").value;
-menuListArray.sort();
-htmldata=""
-for(var i=0;i<menuListArray.length;i++){
-    htmldata=htmldata+ menuListArray[i]+'<br>';
-}
-document.getElementById("displayAddeMenu").innerHTML = htmldata;
+//Completar o código
 
 }
 
 function addTop(){
-var item=document.getElementById("addItem").value;
-menuListArray.push(item);
-addItem();
+//Completar o código
 }
